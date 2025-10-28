7cf7712843beaa1df0aed3bc17b902bb0dd1dc6a
AlexandreMykyta <alexandre.salyha@grupornm.pt>
2025-10-28 16:48:10 +0000

    Version 28/10/2025

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1543,14 +1543,13 @@ sap.ui.define([
                                         sap.m.MessageBox.warning(sResponse);
                                         that.handleLogChange("Aviso na criação da despesa", "", "", sResponse);
                                     } else {
-                                        that.getCardValues();
                                         that.handleLogChange("Sucesso na criação da despesa");
                                     }
                                 }
                             } catch (error) {
 
                             }
-
+                            that.getCardValues();
                             that.handleSuccessSubmit();
                             oModel.refresh(true);
                         },
